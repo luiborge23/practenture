7fc355c1457835a25923320a8d8daf41f4462518
Luis Borges <lborgesm@meta.com>
2026-04-03 09:49:13 -0500

    Initial Commit

---
+++ BizSimAI/BizSimAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizSimAIApp.swift
+//  BizSimAI
+//
+//  Created by Luis Borges on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BizSimAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
